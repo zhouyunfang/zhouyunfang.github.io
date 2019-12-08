20ad458110e4582fbd2c74a3dee63e402bea9f3a
周云芳 <164591357@qq.com>
2019-12-08 14:22:16 +0800

    配置路径

---
+++ react-redux-demo1/precache-manifest.e685a0a2a03cfbf02f1b4f4f76d90571.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "733e38e2caf4347695387239e70645ad",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3c5662f9a7d736ef7588",
+    "url": "./static/css/main.76aee75c.chunk.css"
+  },
+  {
+    "revision": "4271bed6ab0ed65441e9",
+    "url": "./static/js/2.bfa8b503.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.bfa8b503.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3c5662f9a7d736ef7588",
+    "url": "./static/js/main.f4a54d80.chunk.js"
+  },
+  {
+    "revision": "423728abdad4e2f08baa",
+    "url": "./static/js/runtime-main.db865785.js"
+  }
+]);
--- react-redux-demo1/service-worker.js
+++ react-redux-demo1/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.673c5dc5921c279b6247787c956fb3f1.js"
+  "./precache-manifest.e685a0a2a03cfbf02f1b4f4f76d90571.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
