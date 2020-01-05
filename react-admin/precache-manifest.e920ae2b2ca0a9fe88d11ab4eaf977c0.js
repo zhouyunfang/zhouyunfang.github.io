6f962efb71babd3da1d0b500b658b2f873f56737
zhouyunfang <164591357@qq.com>
2020-01-05 17:49:14 +0800

    新增react开发的后台项目

---
+++ react-admin/precache-manifest.e920ae2b2ca0a9fe88d11ab4eaf977c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86fc110317f71b8114820b6c928e51f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "114893549cbc9fe0b85d",
+    "url": "/static/css/2.1cd95810.chunk.css"
+  },
+  {
+    "revision": "5f0f56eb13731738adb5",
+    "url": "/static/css/main.458c9145.chunk.css"
+  },
+  {
+    "revision": "114893549cbc9fe0b85d",
+    "url": "/static/js/2.755db3c6.chunk.js"
+  },
+  {
+    "revision": "4aa0fe1eaee3dcf45338a38641d40f08",
+    "url": "/static/js/2.755db3c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5f0f56eb13731738adb5",
+    "url": "/static/js/main.cc7e6905.chunk.js"
+  },
+  {
+    "revision": "7eff54c81a66d5352648",
+    "url": "/static/js/runtime-main.bc8cf1f9.js"
+  }
+]);
